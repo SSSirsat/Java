c105ef7942dd5d6e3d3071f2e16b1532db818c79
Shubham Suryakant Sirsat <shubham.sirsat321@gmail.com>
2022-12-16 12:20:01 +0530

    kkk

--- java coading/src/Main.java
+++ java coading/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
     }
 }
+
